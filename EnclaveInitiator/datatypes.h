72508f2ad880858373338bfb3dbf70a3bfb05fa4
Ali Abbasi Tadi <humanworth@gmail.com>
2023-08-14 17:14:14 -0400

    changes to number of cells

--- EnclaveInitiator/datatypes.h
+++ EnclaveInitiator/datatypes.h
@@ -11,7 +11,7 @@
 #define N_ROWS             10
 #define N_COLS             4
 #define N_CLUSTERS         3
-#define N_ROWS_PER_WORKER  3000    
+#define N_ROWS_PER_WORKER  1200    
 
 #define DH_KEY_SIZE        20
 #define NONCE_SIZE         16
